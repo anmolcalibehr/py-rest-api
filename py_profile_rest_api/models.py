cb7e6c20195b5f16460070d2924c32a12d58cf93
Anmol Vishwakarma <caseyanmol@gmail.com>
2020-06-13 18:09:31 +0530

    Changes Done in Models_Py iuser to user line25

--- py_profile_rest_api/models.py
+++ py_profile_rest_api/models.py
@@ -7,22 +7,22 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """ Manager for UserProfiles """ 
 
-def create_user(self, email , name , password = None):
+def create_user(self, email , name , password=None):
     """ Create a New User Profile """
     if not email:
         raise ValueError('User Must Enter an Email Address')
 
     email = self.normalize_email(email)
-    user  = self.model(email = email, name  = name)
+    user  = self.model(email=email, name=name)
 
     user.set_password(password)
     user.save()
-
+ 
     return user
 
 def create_superuser(self , email , name , password):
     """ Create a New Super User with Fewer Details"""
-    iuser = self.create_user(email, name , password)
+    user = self.create_user(email, name , password)
 
     user.is_superuser = True 
     user.is_staff = True
