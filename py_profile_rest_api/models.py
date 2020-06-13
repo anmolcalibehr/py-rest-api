5e06ce887428316038599b5a96927b4023940771
Anmol Vishwakarma <caseyanmol@gmail.com>
2020-06-13 18:25:06 +0530

    Added auth.models - AbstractUser , UserManager in models.py file

--- py_profile_rest_api/models.py
+++ py_profile_rest_api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
